456df3aa1a1a8c4bd69fb2f42385a18aac386d12
BalavinayakamD <balavinayakamd.al24@bitsathy.ac.in>
2024-10-23 15:28:28 +0530

    WIP

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function datentime(){
     const display = d.toLocaleString();
     document.querySelector(".date").textContent = display;
 }
-setInterval(datentime,1000); 
+setInterval(datentime,1000);  
+
+function taskadd(){
+    var ul = document.getElementById("unol");
+    var inp = document.getElementById("task");
+    var list = document.createElement("li");
+    list.innerHTML = inp.value
+    ul.append(list)
+}
